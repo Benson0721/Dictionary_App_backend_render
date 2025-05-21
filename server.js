baf2c111abc654b68776861825632f7b6b2bab16
Mpass <121707487+ME942082@users.noreply.github.com>
2025-05-21 15:39:22 +0800

    add vercel url

--- server.js
+++ server.js
@@ -11,29 +11,28 @@ import express from "express";
 import LocalStrategy from "passport-local";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import cors from "cors";
 
 // 獲取當前檔案的路徑
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   // 動態解析 .env 的絕對路徑
-const envPath = path.resolve(__dirname, ".env");
-console.log("Loading .env from:", envPath);
+  const envPath = path.resolve(__dirname, ".env");
+  console.log("Loading .env from:", envPath);
 
-// 檢查 .env 檔案是否存在
-if (!fs.existsSync(envPath)) {
-  throw new Error(`.env file not found at: ${envPath}`);
-}
+  // 檢查 .env 檔案是否存在
+  if (!fs.existsSync(envPath)) {
+    throw new Error(`.env file not found at: ${envPath}`);
+  }
 
-// 加載環境變數
-const result = dotenv.config({ path: envPath });
-if (result.error) {
-  throw new Error(`Failed to load .env: ${result.error.message}`);
+  // 加載環境變數
+  const result = dotenv.config({ path: envPath });
+  if (result.error) {
+    throw new Error(`Failed to load .env: ${result.error.message}`);
+  }
 }
-}
-
 
 const DBURL = process.env.DB_URL;
 if (!DBURL) {
@@ -68,7 +67,30 @@ store.on("error", function (e) {
 });
 
 const app = express();
+
 const port = process.env.PORT || 5000;
+
+app.set("trust proxy", 1);
+
+const whiteList = [
+  "https://dictionary-app-frontend-vercel.vercel.app",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // 多網址
+      if (!origin || whiteList.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 const sessionConfig = {
   store,
   name: "session",
@@ -78,12 +100,14 @@ const sessionConfig = {
   unset: "destroy",
   cookie: {
     httpOnly: true,
-    secure: false,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production",
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
-app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use(express.static(path.join(__dirname, "../dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sessionConfig));
@@ -94,7 +118,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use("/api", UserRoutes);
   app.use("/api", FavRoutes);
 } else {
@@ -102,15 +126,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use("/", FavRoutes);
 }
 
-
 app.get("/wakeup", (req, res) => {
   console.log("ok!ok!....I am wakeup!");
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.send("Allright...I have wake up...");
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
